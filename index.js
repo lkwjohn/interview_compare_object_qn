2df74bb6c7e2932b3346310a84917aa06e40ad1c
John Lee <>
2019-10-27 00:57:30 +0800

    Added new logic to handle multi level comparsion

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const isObjectEqual = (obj1, obj2) => {
-    const obj1Keys = Object.keys(obj1);
-    const obj2Keys = Object.keys(obj2);
+    try {
+        const obj1Keys = Object.keys(obj1);
+        const obj2Keys = Object.keys(obj2);
+        let isEqual = true;
 
-    if (obj1Keys.length !== obj2Keys.length) {
-        return false;
-    }
-
-    for (let key of obj1Keys) {
-        if (obj1[key] !== obj2[key]) {
+        if (obj1Keys.length !== obj2Keys.length) {
             return false;
         }
+
+        for (let key of obj1Keys) {
+            if (typeof obj1[key] === 'object') {
+                isEqual = isEqual && isObjectEqual(obj1[key], obj2[key]);
+            }
+            else if (obj1[key] !== obj2[key]) {
+                isEqual = false;
+            }
+        }
+
+        return isEqual;
+    }
+    catch (err) {
+        return false
     }
 
-    return true;
 }
 
 module.exports = isObjectEqual;
--- index.test.js
+++ index.test.js
@@ -1,4 +1,5 @@
 const isObjectEqual = require('./index');
+const fs = require('fs');
 
 describe('isObjectEqual', () => {
 
@@ -54,5 +55,132 @@ describe('isObjectEqual', () => {
         const thirdComparisionResult = isObjectEqual(obj1, obj4);
         expect(thirdComparisionResult).toBeFalsy();
     });
+
+    test('Compare two object keys and value(s) on mutiple level', () => {
+        const obj1 = {
+            keyString: 'some string',
+            keyNumber: 10,
+            keyObject: {
+                food: 'some food'
+            }
+        }
+
+        const obj2 = {
+            keyString: 'some string',
+            keyNumber: 10,
+            keyObject: {
+                food: 'some food'
+            }
+        }
+
+        const obj3 = {
+            keyString: 'some string',
+            keyNumber: 10,
+            keyObject: {
+                food: 'japan food'
+            }
+        }
+
+        const obj4 = {
+            keyString: 'some string',
+            keyNumber: 10,
+            keyObject: {
+                interest: 'football'
+            }
+        }
+
+        const obj5 = {
+            keyString: 'some string',
+            keyNumber: 10,
+            keyObjectLevel1: {
+                keyObjectLevel2: {
+                    food: 'some food'
+                }
+            }
+        }
+
+        const obj6 = {
+            keyString: 'some string',
+            keyNumber: 10,
+            keyObjectLevel1: {
+                keyObjectLevel2: {
+                    food: 'some food'
+                }
+            }
+        }
+
+        const obj7 = {
+            keyString: 'some string',
+            keyNumber: 10,
+            keyObjectLevel1: {
+                keyObjectLevel2: {
+                    food: 'not the same food'
+                }
+            }
+        }
+
+        const obj8 = {
+            keyString: 'some string',
+            keyNumber: 10,
+            keyObjectLevel1: {
+                keyObjectLevel2: {
+                    food: 'same food'
+                }
+            },
+            keyObject2Level1: {
+                keyObjectLevel2: {
+                    food: 'same food'
+                }
+            }
+        }
+
+        const obj9 = {
+            keyString: 'some string',
+            keyNumber: 10,
+            keyObjectLevel1: {
+                keyObjectLevel2: {
+                    food: 'same food'
+                }
+            },
+            keyObject2Level1: {
+                keyObjectLevel2: {
+                    food: 'not the same food'
+                }
+            }
+        }
+
+        const firstComparisionResult = isObjectEqual(obj1, obj2);
+        expect(firstComparisionResult).toBeTruthy();
+
+        const secondComparisionResult = isObjectEqual(obj1, obj3);
+        expect(secondComparisionResult).toBeFalsy();
+
+        const thirdComparisionResult = isObjectEqual(obj1, obj4);
+        expect(thirdComparisionResult).toBeFalsy();
+
+        const notWithSameStructure = isObjectEqual(obj5, obj4);
+        expect(notWithSameStructure).toBeFalsy();
+
+        const withSameStructure = isObjectEqual(obj5, obj6);
+        expect(withSameStructure).toBeTruthy();
+
+        const withSameStructureButDiffValue = isObjectEqual(obj5, obj7);
+        expect(withSameStructureButDiffValue).toBeFalsy();
+
+        const withSameButMoreStructure = isObjectEqual(obj8, obj9);
+        expect(withSameButMoreStructure).toBeFalsy();
+
+    });
+
+    test('Compare using package-lock.,json', () => {
+        const packageLock1 = JSON.parse(fs.readFileSync('package-lock.json'));
+        const notTheSamepackageLock = JSON.parse(fs.readFileSync('not-the-same-package-lock.json'));
+
+        const comparePackageLock = isObjectEqual(packageLock1, packageLock1);
+        expect(comparePackageLock).toBeTruthy();
+
+        const compareDiffPackageLock = isObjectEqual(packageLock1, notTheSamepackageLock);
+        expect(compareDiffPackageLock).toBeFalsy();
+    })
 
 })
